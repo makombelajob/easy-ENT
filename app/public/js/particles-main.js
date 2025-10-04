2c6b3ac9249e7144738c6b8da7caadddce3872a0
makombelajob <makombelajob@gmail.com>
2025-10-04 19:19:24 +0200

    adding practicles-js

---
+++ app/public/js/particles-main.js
@@ -0,0 +1,32 @@
+particlesJS('particles-main', {
+    "particles": {
+        "number": {
+            "value": 50
+        },
+        "shape": {
+            "type": "triangle",
+            "stroke": {
+                "width": 0,
+                "color": "#ff0000"
+            }
+        },
+        "size": {
+            "value": 10
+        },
+        "color": {
+            "value": "#ff0000"
+        },
+        "move": {
+            "enable": true,
+            "speed": 2
+        }
+    },
+    "interactivity": {
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "repulse"
+            }
+        }
+    }
+});
